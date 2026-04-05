48b2b05b699baf77b02a907934d4df232ce4e221
Snowky92 <amaurypinheiro@hotmail.fr>
2026-04-05 14:49:39 +0200

    ajout timer + update script

--- testcam.gd
+++ testcam.gd
@@ -6,13 +6,12 @@ extends Camera3D
 @onready var theo_3 = $"../Audio/theo3"
 @onready var theo_4 = $"../Audio/theo4"
 
-
 func _input(event):
 	if event is InputEventKey and event.keycode == KEY_ENTER and event.pressed:
 		_start_sequence()
 
 func _start_sequence():
-	# Choisir une image random et l'appliquer
+	# Choisir une image random
 	var images = ["champidead", "chat", "diamond", "duck"]
 	var chosen = images[randi() % images.size()]
 	var texture = load("res://assets/Images/" + chosen + ".png")
@@ -35,7 +34,6 @@ func _start_sequence():
 	get_node("../Objects/Monitor/Monitor/MeshInstance3D").visible = false
 	img.visible = false
 	
-	#ici theo parle 
 	await get_tree().create_timer(1.0).timeout
 	_do_traveling()
 
@@ -49,7 +47,23 @@ func _do_traveling():
 	lamp_switch.play()
 	get_node("../Objects/lamp_desk2/SpotLight3D").visible = true
 	
-	#dialogue de thep
+	# theo
 	var dialogues = [theo_1, theo_2, theo_3, theo_4]
 	var chosen_dialogue = dialogues[randi() % dialogues.size()]
 	chosen_dialogue.play()
+	
+	# timer
+	var label = get_node("../Objects/Digital clock/Digital_clock_Cube/MeshInstance3D/SubViewport/RichTextLabel")
+	print(get_node("../Objects/Digital clock"))
+	
+	_start_timer(label)
+	
+func _start_timer(label):
+	var time_left = 90
+	while time_left >= 0:
+		var minutes = time_left / 60
+		var seconds = time_left % 60
+		label.text = "%02d:%02d" % [minutes, seconds]
+		await get_tree().create_timer(1.0).timeout
+		time_left -= 1
+	print("test")
