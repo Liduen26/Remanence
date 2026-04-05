9aabc456e64c410844d47cc8e862b4d75f5742ca
Liduen <75698473+Liduen26@users.noreply.github.com>
2026-04-05 15:21:53 +0200

    Improve Gamemanager and refacto

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -6,6 +6,10 @@ class_name GameManager
 @onready var camera_3d: Camera3D = $"../Camera3D"
 @onready var level_timer: Timer = $LevelTimer
 
+@export var game_time_sec := 60
+
+var time_left := 0
+
 signal high_score_updated(score: int)
 var high_score: int = 3:
 	set(new_score):
@@ -15,35 +19,66 @@ var high_score: int = 3:
 
 func _ready() -> void:
 	self.high_score = load_high_score()
+	self.time_left = game_time_sec
 
 func _input(event):
 	if Input.is_action_just_pressed("Start"):
 		_start_sequence()
+	if Input.is_action_just_pressed("Analyse"):
+		_stop_sequence()
+
+
 
 func _start_sequence():
-	#affiche img
-	get_node("../Objects/Monitor/Monitor/img").visible = true
+	# Choisir une image random
+	var images = ["champidead", "chat", "diamond", "duck"]
+	var chosen = images[randi() % images.size()]
+	var texture = load("res://assets/Images/" + chosen + ".png")
 	
-	#attend
+	var img = get_node("../Objects/Monitor/Monitor/img")
+	img.get_surface_override_material(0).albedo_texture = texture
+	
+	# Affiche img
+	var viewport = get_node("../Objects/Monitor/Monitor/MeshInstance3D/SubViewport")
+	viewport.get_node("TitleLabel").visible = false
+	viewport.get_node("StartLabel").visible = false
+	viewport.get_node("ScoreLabel").visible = false
+	img.visible = true
+	
+	# Attend
 	await get_tree().create_timer(3.0).timeout
 	
-	#eteint ecran
+	# Eteint écran
 	pcdie.play()
 	get_node("../Objects/Monitor/Monitor/MeshInstance3D").visible = false
-	get_node("../Objects/Monitor/Monitor/img").visible = false
+	img.visible = false
 	
 	await get_tree().create_timer(1.0).timeout
-	
 	level_timer.start()
 	camera_3d._do_traveling()
 
+func _stop_sequence():
+	camera_3d._reverse_traveling()
+	# Affiche img
+	get_node("../Objects/Monitor/Monitor/MeshInstance3D").visible = true
+	var viewport = get_node("../Objects/Monitor/Monitor/MeshInstance3D/SubViewport")
+	viewport.get_node("TitleLabel").visible = true
+	viewport.get_node("StartLabel").visible = true
+	viewport.get_node("ScoreLabel").visible = true
 
-# Fin de la partie
+
 func _on_level_timer_timeout() -> void:
-	
-	pass
-
-
+	if time_left > 0:
+		var minutes = time_left / 60
+		var seconds = time_left % 60
+		# timer
+		var label = get_node("../Objects/Digital clock/Digital_clock_Cube/MeshInstance3D/SubViewport/RichTextLabel")
+		#print(get_node("../Objects/Digital clock"))
+		label.text = "%02d:%02d" % [minutes, seconds]
+		time_left -= 1
+		
+	elif time_left <= 0:
+		_stop_sequence()
 
 
 func _on_drawing_controller_score_updated(score: int) -> void:
@@ -52,9 +87,6 @@ func _on_drawing_controller_score_updated(score: int) -> void:
 	if score > high_score:
 		high_score = score
 	pass # Replace with function body.
-
-
-
 
 
 
--- testcam.gd
+++ testcam.gd
@@ -6,36 +6,6 @@ extends Camera3D
 @onready var theo_3 = $"../Audio/theo3"
 @onready var theo_4 = $"../Audio/theo4"
 
-func _input(event):
-	if event is InputEventKey and event.keycode == KEY_ENTER and event.pressed:
-		_start_sequence()
-
-func _start_sequence():
-	# Choisir une image random
-	var images = ["champidead", "chat", "diamond", "duck"]
-	var chosen = images[randi() % images.size()]
-	var texture = load("res://assets/Images/" + chosen + ".png")
-	
-	var img = get_node("../Objects/Monitor/Monitor/img")
-	img.get_surface_override_material(0).albedo_texture = texture
-	
-	# Affiche img
-	var viewport = get_node("../Objects/Monitor/Monitor/MeshInstance3D/SubViewport")
-	viewport.get_node("TitleLabel").visible = false
-	viewport.get_node("StartLabel").visible = false
-	viewport.get_node("ScoreLabel").visible = false
-	img.visible = true
-	
-	# Attend
-	await get_tree().create_timer(3.0).timeout
-	
-	# Eteint écran
-	pcdie.play()
-	get_node("../Objects/Monitor/Monitor/MeshInstance3D").visible = false
-	img.visible = false
-	
-	await get_tree().create_timer(1.0).timeout
-	_do_traveling()
 
 func _do_traveling():
 	var tween = create_tween()
@@ -52,26 +22,15 @@ func _do_traveling():
 	var chosen_dialogue = dialogues[randi() % dialogues.size()]
 	chosen_dialogue.play()
 	
-	# timer
-	var label = get_node("../Objects/Digital clock/Digital_clock_Cube/MeshInstance3D/SubViewport/RichTextLabel")
-	print(get_node("../Objects/Digital clock"))
 	
-	_start_timer(label)
-	
-func _start_timer(label):
-	var time_left = 90
-	while time_left >= 0:
-		var minutes = time_left / 60
-		var seconds = time_left % 60
-		label.text = "%02d:%02d" % [minutes, seconds]
-		await get_tree().create_timer(1.0).timeout
-		time_left -= 1
-	print("test")
-	_reverse_traveling()
-	
+
+
 func _reverse_traveling():
 	var tween = create_tween()
 	tween.set_parallel(true)
 	tween.tween_property(self, "position:z", 0.7, 1.5)
 	tween.tween_property(self, "rotation_degrees:x", -17.5, 1.5)
 	get_node("../Objects/lamp_desk2/SpotLight3D").visible = false
+
+
+	
