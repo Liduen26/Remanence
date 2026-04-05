8398c23bc408cee2a0995449c2a78086f84f73db
Snowky92 <amaurypinheiro@hotmail.fr>
2026-04-05 14:21:04 +0200

    ajout des img et des dialogues

--- testcam.gd
+++ testcam.gd
@@ -1,6 +1,10 @@
 extends Camera3D
 @onready var lamp_switch = $"../Audio/Lamp_switch"
 @onready var pcdie = $"../Audio/pcdie"
+@onready var theo_1 = $"../Audio/theo1"
+@onready var theo_2 = $"../Audio/theo2"
+@onready var theo_3 = $"../Audio/theo3"
+@onready var theo_4 = $"../Audio/theo4"
 
 
 func _input(event):
@@ -8,28 +12,44 @@ func _input(event):
 		_start_sequence()
 
 func _start_sequence():
-		#affiche img
-	get_node("../Objects/Monitor/Monitor/img").visible = true
+	# Choisir une image random et l'appliquer
+	var images = ["champidead", "chat", "diamond", "duck"]
+	var chosen = images[randi() % images.size()]
+	var texture = load("res://assets/Images/" + chosen + ".png")
 	
-	#attend
+	var img = get_node("../Objects/Monitor/Monitor/img")
+	img.get_surface_override_material(0).albedo_texture = texture
+	
+	# Affiche img
+	var viewport = get_node("../Objects/Monitor/Monitor/MeshInstance3D/SubViewport")
+	viewport.get_node("TitleLabel").visible = false
+	viewport.get_node("StartLabel").visible = false
+	viewport.get_node("ScoreLabel").visible = false
+	img.visible = true
+	
+	# Attend
 	await get_tree().create_timer(3.0).timeout
 	
-	#etein ecran
+	# Eteint écran
 	pcdie.play()
 	get_node("../Objects/Monitor/Monitor/MeshInstance3D").visible = false
-	get_node("../Objects/Monitor/Monitor/img").visible = false
+	img.visible = false
 	
+	#ici theo parle 
 	await get_tree().create_timer(1.0).timeout
 	_do_traveling()
-	
-
 
 func _do_traveling():
 	var tween = create_tween()
-	tween.set_parallel(true)  
+	tween.set_parallel(true)
 	tween.tween_property(self, "position:z", 0.85, 1.5)
 	tween.tween_property(self, "rotation_degrees:x", -55.0, 1.5)
 	
 	await get_tree().create_timer(1.0).timeout
 	lamp_switch.play()
 	get_node("../Objects/lamp_desk2/SpotLight3D").visible = true
+	
+	#dialogue de thep
+	var dialogues = [theo_1, theo_2, theo_3, theo_4]
+	var chosen_dialogue = dialogues[randi() % dialogues.size()]
+	chosen_dialogue.play()
